63c14eb154ed8b6f5406aebdcbffa853890d57c0
Tammashicks <tammas@uoregon.edu>
2021-04-20 20:53:57 -0700

    basic working dialogue system
    implemented the basic concepts of a working dialogue system. This is more so that I can have an idea of how to implement dialogue in the game. Next up is triggers.

--- Assets/Scripts/DialogueScripts/DialogueManager.cs
+++ Assets/Scripts/DialogueScripts/DialogueManager.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
-    private Queue<string> stentences;
+    public Text nameText;
+    public TMP_Text dialogueText;
+    private Queue<string> sentences;
+    public GameObject background;
+    private string currentSentence;
 
     private void Start()
     {
-        stentences = new Queue<string>();
+        sentences = new Queue<string>();
     }
 
     public void StartDialogue(Dialogue dialogue)
     {
-        Debug.Log("Starting conversation with " + dialogue.characterName);
+        background.SetActive(true);
+        nameText.text = dialogue.characterName;
+        
+        sentences.Clear();
+
+        foreach (string words in dialogue.stenences)
+        {
+            sentences.Enqueue(words);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        currentSentence = sentences.Dequeue();
+
+        dialogueText.text = currentSentence;
+    }
+
+    public void EndDialogue()
+    {
+        background.SetActive(false);
     }
 }
