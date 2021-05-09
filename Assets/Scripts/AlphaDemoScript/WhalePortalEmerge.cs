0bc16cdacf407ec8fdc1ae0eb9b33384cd11becc
Tammashicks <tammas@uoregon.edu>
2021-05-09 13:30:30 -0700

    Whale working

--- Assets/Scripts/AlphaDemoScript/WhalePortalEmerge.cs
+++ Assets/Scripts/AlphaDemoScript/WhalePortalEmerge.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class WhalePortalEmerge : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // the whale which will be activated when the player 
+    public WhaleMovement whaleActivity;
+    // this determines how long the portal lasts
+    public float portalDuration;
+    // this is the timer used to track duration
+    private float timer = 0;
+    // bool used to alternate timer;
+    private bool playerHasEntered;
+    // the portal it emerges from;
+    public GameObject portal;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "Player")
+        {
+            whaleActivity.enabled = true;
+            playerHasEntered = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        timer = 0;
+        whaleActivity.enabled = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (playerHasEntered)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if (timer >= portalDuration)
+        {
+            portal.SetActive(false);
+            this.enabled = false;
+        }
     }
 }
